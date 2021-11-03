6f0260b52efb2498eca7fa330319c03573f8a467
Tomás Henrique Batista Aguiar <tomas.h.aguiar@accenture.com>
2021-11-03 12:08:09 -0300

    first commit

---
+++ TestProject1Financial.Organizer.Api.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TestProject1Financial.Organizer.Api.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
